2c5fe1253d099da0e272495a86755692254dee2a
chaitanya <chaitanyaharshan.official@gmail.com>
2025-01-20 14:15:35 +0530

    added hasing folder in concepts and did a 2d hasing sum is important very brain power intensive

---
+++ __concepts/hashing/first_completely_painted_row_or_col.java
@@ -0,0 +1,69 @@
+package __concepts.hashing;
+
+public class first_completely_painted_row_or_col {
+
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+
+        int[][] position = new int[n*m + 1][2];
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                int element_idx = mat[i][j];
+                position[element_idx][0] = i;
+                position[element_idx][1] = j;
+            }
+        }
+
+        for (int i=0; i<arr.length; i++) {
+            int row_idx = position[arr[i]][0];
+            rows[row_idx]++;
+            if (rows[row_idx] == n) return i;
+
+            int col_idx = position[arr[i]][1];
+            cols[col_idx]++;
+            if (cols[col_idx] == m) return i;
+        }
+        return -1;
+    }
+}
+
+/*
+ * 2661. First Completely Painted Row or Column
+https://leetcode.com/problems/first-completely-painted-row-or-column/description/
+
+You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
+
+Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i].
+
+Return the smallest index i at which either a row or a column will be completely painted in mat.
+
+ 
+
+Example 1:
+
+image explanation for example 1
+Input: arr = [1,3,4,2], mat = [[1,4],[2,3]]
+Output: 2
+Explanation: The moves are shown in order, and both the first row and second column of the matrix become fully painted at arr[2].
+Example 2:
+
+image explanation for example 2
+Input: arr = [2,8,7,4,1,3,5,6,9], mat = [[3,2,5],[1,4,6],[8,7,9]]
+Output: 3
+Explanation: The second column becomes fully painted at arr[3].
+ 
+
+Constraints:
+
+m == mat.length
+n = mat[i].length
+arr.length == m * n
+1 <= m, n <= 105
+1 <= m * n <= 105
+1 <= arr[i], mat[r][c] <= m * n
+All the integers of arr are unique.
+All the integers of mat are unique.
+ */
--- __concepts/parenthesis/longest_valid_parenthesis.java
+++ __concepts/parenthesis/longest_valid_parenthesis.java
@@ -63,24 +63,20 @@ public class longest_valid_parenthesis {
  * 32. Longest Valid Parentheses
 https://leetcode.com/problems/longest-valid-parentheses/
 
-Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
-substring
-.
-
- 
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
 
 Example 1:
-
 Input: s = "(()"
 Output: 2
 Explanation: The longest valid parentheses substring is "()".
+
 Example 2:
 
 Input: s = ")()())"
 Output: 4
 Explanation: The longest valid parentheses substring is "()()".
-Example 3:
 
+Example 3:
 Input: s = ""
 Output: 0
  
