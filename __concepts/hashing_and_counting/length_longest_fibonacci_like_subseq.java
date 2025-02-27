6ff88b58634d77d7c86daf8f06bbeca28679bf05
chaitanya <chaitanyaharshan.official@gmail.com>
2025-02-27 12:43:32 +0530

    length of longest finonacci like subsequence

---
+++ __concepts/hashing_and_counting/length_longest_fibonacci_like_subseq.java
@@ -0,0 +1,93 @@
+package __concepts.hashing_and_counting;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+// both look same TBH and not nuch diff
+public class length_longest_fibonacci_like_subseq {
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length;
+        int max = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i: arr) set.add(i);
+
+        for (int i=0; i<n-1; i++) {
+            for (int j=i+1; j<n; j++) {
+                int cnt = 0;
+                int prev = arr[i], cur = arr[j];
+                int nxt = prev + cur;
+                if (set.contains(nxt)) cnt = 3;
+                else continue;
+
+                prev = cur;
+                cur = nxt;
+
+                while (set.contains(prev + cur)) {
+                    nxt = prev + cur;
+                    prev = cur;
+                    cur = nxt;
+
+                    cnt++;
+                }
+
+                max = Math.max(max, cnt);
+            }
+        }
+        return max;
+    }
+
+    // DP
+    public int lenLongestFibSubseq1(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<n; i++) map.put(arr[i], i);
+
+        int[][] dp = new int[n][n];
+        int max = 0;
+
+        for (int i=n-1; i>=0; i--) {
+            for (int j=0; j<i; j++) {
+                int prev = arr[j], cur = arr[i];
+                int nxt = prev + cur;
+                if (map.containsKey(nxt)) {
+                    dp[i][j] = dp[map.get(nxt)][map.get(cur)] + 1;
+                    max = Math.max(max, dp[i][j] + 2);
+                }
+            }
+        }
+        return max;
+    }
+}
+
+
+/*
+ * 873. Length of Longest Fibonacci Subsequence
+https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/description/
+
+A sequence x1, x2, ..., xn is Fibonacci-like if:
+
+n >= 3
+xi + xi+1 == xi+2 for all i + 2 <= n
+Given a strictly increasing array arr of positive integers forming a sequence, return the length of the longest Fibonacci-like subsequence of arr. If one does not exist, return 0.
+
+A subsequence is derived from another sequence arr by deleting any number of elements (including none) from arr, without changing the order of the remaining elements. For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5,6,7,8]
+Output: 5
+Explanation: The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+Example 2:
+
+Input: arr = [1,3,7,11,12,14,18]
+Output: 3
+Explanation: The longest subsequence that is fibonacci-like: [1,11,12], [3,11,14] or [7,11,18].
+ 
+
+Constraints:
+
+3 <= arr.length <= 1000
+1 <= arr[i] < arr[i + 1] <= 109
+ */
