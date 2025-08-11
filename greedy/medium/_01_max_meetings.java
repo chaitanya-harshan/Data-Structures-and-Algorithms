7c0526a21d48418e0cec861d32ac0552eed5f4ee
chaitanya <chaitanyaharshan.official@gmail.com>
2025-08-11 21:47:30 +0530

    greedy medium started

--- greedy/medium/_01_max_meetings.java
+++ greedy/medium/_01_max_meetings.java
@@ -3,11 +3,13 @@
  * For an index ‘i’, start[i] denotes the starting time of the ith meeting while end[i]  
  * will denote the ending time of the ith meeting. Find the maximum number of meetings that 
  * can be accommodated if only one meeting can happen in the room at a  particular time.
+ * 
+ * https://www.naukri.com/code360/problems/maximum-meetings_1062658
+ * 
  */
 package greedy.medium;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 
 public class _01_max_meetings {
@@ -15,51 +17,26 @@ public class _01_max_meetings {
     public static void main(String[] args) {
         int[] start = {1,3,0,5,8,5};
         int[] end = {2,4,6,7,9,9};
-        System.out.println(maxMeetings(start, end, start.length));
+        System.out.println(maximumMeetings(start, end));
     }
     
-    public static int maxMeetings(int start[], int end[], int n)
-    {
-        meeting[] meets = new meeting[n];
+    public static int maximumMeetings(int []start, int []end) {
+        int n = start.length;
+        int[][] meet = new int[n][2];
         for (int i=0; i<n; i++) {
-            meets[i] = new meeting(start[i], end[i], i+1);
-            // meets[i].start = ;
-            // meets[i].end = end[i];
-            // meets[i].pos = i+1;
-        }
-        
-        int count = 1;
-        // Arrays.sort(meets, new meetingComparator());
-        Arrays.sort(meets, (a,b) -> a.end - b.end);
-        int time = meets[0].end;
-        for (int i=1; i<meets.length; i++) {
-            if (meets[i].start > time) {
-                count++;
-                time = meets[i].end;
+            meet[i][0] = start[i];
+            meet[i][1] = end[i];
+        }        
+
+        Arrays.sort(meet, (a,b) -> a[1]-b[1]);
+        int meetings = 0;
+        int avaliable_since = 0;
+        for (int[] meeting: meet) {
+            if (meeting[0] > avaliable_since) {
+                meetings++;
+                avaliable_since = meeting[1];
             }
         }
-        return count;
-    }
-}
-
-class meeting {
-    int start;
-    int end;
-    int pos; // incase if they ask for the meeitng numbers(positions)
-    
-    meeting(int start, int end, int pos) {
-        this.start = start;
-        this.end = end;
-        this.pos = pos;
-    }
-}
-
-
-// sorting based on the end time in ascending order
-class meetingComparator implements Comparator<meeting> {
-    @Override
-    public int compare(meeting a, meeting b) {
-        if (a.end <= b.end) return -1;
-        else return 1;
+        return meetings;
     }
 }
---
+++ greedy/medium/_03_jump_game_2.java
@@ -0,0 +1,102 @@
+package greedy.medium;
+
+public class _03_jump_game_2 {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+        int jumps = 0, maxReach = 0, currentEnd = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = maxReach;
+                if (currentEnd >= n - 1) break;
+            }
+        }
+        
+        return jumps;
+    }
+
+    public int jump1(int[] nums) {
+        int curEnd = 0; // not left (this is also right)
+        int far = 0; // furthest right (boundary)
+        int n = nums.length;
+        int jumps = 0;
+
+        for (int i=0; i<n-1; i++) {
+            far = Math.max(far, i + nums[i]);
+            if (far >= n-1) return jumps + 1;
+            if (i == curEnd) { // current window is expended so now it's time to jump to the nxt window
+                jumps++;
+                curEnd = far;
+            }
+        }
+        return jumps;
+    }
+
+
+    // recursion TLE: O(n^2) | S: O(n)
+    // ___________________________________________________________________________________________ 
+
+    int n;
+    int[] nums;
+    int[] dp;
+
+    public int jump2(int[] nums) {
+        this.nums = nums;
+        this.n = nums.length;
+        this.dp = new int[n];
+
+        return dfs(0);
+    }
+
+    public int dfs(int i) {
+        if (i >= n-1) {
+            return 0;
+        }
+        if (dp[i] != 0) return dp[i];
+
+        int min = n;
+        for (int j=1; j <= nums[i]; j++) {
+            min = Math.min(min, dfs(i+j));
+        }
+        dp[i] = min + 1;
+        return min + 1;
+    }
+}
+
+
+/*
+ * # [45. Jump Game II](https://leetcode.com/problems/jump-game-ii/description/)
+
+You are given a **0-indexed**  array of integers <code>nums</code> of length <code>n</code>. You are initially positioned atindex 0.
+
+Each element <code>nums[i]</code> represents the maximum length of a forward jump from index <code>i</code>. In other words, if you are at index <code>i</code>, you can jump to any index <code>(i + j)</code>where:
+
+- <code>0 <= j <= nums[i]</code> and
+- <code>i + j < n</code>
+
+Return the minimum number of jumps to reach index <code>n - 1</code>. The test cases are generated such that you can reach index<code>n - 1</code>.
+
+**Example 1:** 
+
+```
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+```
+
+**Example 2:** 
+
+```
+Input: nums = [2,3,0,1,4]
+Output: 2
+```
+
+**Constraints:** 
+
+- <code>1 <= nums.length <= 10^4</code>
+- <code>0 <= nums[i] <= 1000</code>
+- It's guaranteed that you can reach <code>nums[n - 1]</code>.
+ */
